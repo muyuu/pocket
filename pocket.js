5f29e471fa86f4a34083e10a8c36d64a1a55fa85
Muyuu Fujita <anticyborg@gmail.com>
2013-11-01 19:26:37 +0900

    IE11

--- pocket.js
+++ pocket.js
@@ -99,15 +99,15 @@ Pocket = function() {
       if (UA.indexOf("msie") !== -1 || UA.indexOf("trident") !== -1) {
         console.log(UA.indexOf("trident"));
         name = "ie";
-        if (VERSION.indexOf("msie 6.") !== -1) {
+        if (VER.indexOf("msie 6.") !== -1) {
           ver = 6;
-        } else if (VERSION.indexOf("msie 7.") !== -1) {
+        } else if (VER.indexOf("msie 7.") !== -1) {
           ver = 7;
-        } else if (VERSION.indexOf("msie 8.") !== -1) {
+        } else if (VER.indexOf("msie 8.") !== -1) {
           ver = 8;
-        } else if (VERSION.indexOf("msie 9.") !== -1) {
+        } else if (VER.indexOf("msie 9.") !== -1) {
           ver = 9;
-        } else if (VERSION.indexOf("msie 10.") !== -1) {
+        } else if (VER.indexOf("msie 10.") !== -1) {
           ver = 10;
         } else {
           if (UA.indexOf("trident") !== -1) {
